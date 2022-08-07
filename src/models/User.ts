7cfb2308da5521a5450ca88706bae90795d42fd1
Victor <vtrownz@hotmail.com>
2022-08-07 18:54:34 -0300

    initial

---
+++ src/models/User.ts
@@ -0,0 +1,10 @@
+import { Field, ID, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export default class User {
+  @Field((_type) => ID)
+  id!: string;
+
+  @Field((_type) => String)
+  name!: string;
+}
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import path from 'path';
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { UserResolver } from './resolvers/userResolver';
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
+  });
+
+  const server = new ApolloServer({ schema });
+
+  const { url } = await server.listen();
+
+  console.log(`Server running on ${url}`);
+};
+
+main();
